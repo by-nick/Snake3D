6dfd3fa4eee4ac3618762bd17cab6184c7967ada
Nick <laribrito78@gmail.com>
2026-08-06 09:47:12 -0300

    Transferindo alterações para outro pc

---
+++ Assets/Scripts/item/ColisoesColeta.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class ItemColetavel : MonoBehaviour
+{
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("item"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
+
