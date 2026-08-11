096db7aacbf93c50a8a7c616a0772a50018eb0fe
by-nick <laribrito78@gmail.com>
2026-08-11 10:27:36 -0300

    adicionando colisoes na parede e consertando rotação da camera com relaçao ao mundo e ao objeto

--- Assets/Scripts/Snake/MovimentoBasico.cs
+++ Assets/Scripts/Snake/MovimentoBasico.cs
@@ -2,25 +2,20 @@ using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     private float vel = 2.5f;
     private Rigidbody RB;
-    
+
     void Start()
     {
-        //pega ref. do componente Rigidbody adicionado no obj
-        RB = GetComponent <Rigidbody>();
+        RB = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-
-        transform.Translate(new Vector3(h,0,v) * vel * Time.deltaTime);
-
-
+        
+        Vector3 mover = new Vector3(h, 0f, v); 
+        transform.Translate(mover * vel * Time.deltaTime);
+        
     }
 }
